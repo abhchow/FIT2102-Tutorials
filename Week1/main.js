7e5d229b430d2041274d9a019ff764bc23e38ffb
Albert Chow <abhchow@gmail.com>
2022-07-25 12:27:43 +1000

    completed tute 1 selfStudy

--- Week1/main.js
+++ Week1/main.js
@@ -1,12 +1,12 @@
 /* 
 Complete the following table when you submit this file:
 
-Surname     | Firstname | email | Contribution% | Any issues?
-=============================================================
-Person 1... |           |       | 25%           |
-Person 2... |           |       | 25%           |
-Person 3... |           |       | 25%           |
-Person 4... |           |       | 25%           |
+Surname     | Firstname | email                       | Contribution% | Any issues?
+===================================================================================
+Chow        | Albert    | acho0023@student.monash.edu | 25%           |
+Person 2... |           |                             | 25%           |
+Person 3... |           |                             | 25%           |
+Person 4... |           |                             | 25%           |
 
 complete Worksheet 1 by entering code in the places marked below...
 
@@ -22,12 +22,12 @@ browser window to run the test.
     respectively.
     Create a mutable variable called ‘aVariable’ and assign its value to 1.
 */
-// your first line of code here...
+let aVariable = 1;
 
 /*
     Create an immutable variable called ‘aConst’ and assign its value to aVariable + 1.
 */
-// your second line of code here...
+const aConst = aVariable + 1;
 
 /**
     Exercise 2:
@@ -35,41 +35,80 @@ browser window to run the test.
     Inside the function create another variable called 'anotherVariable',
     set its value to 2 and return anotherVariable.
 */
+function aFunction() {
+    let anotherVariable = 2;
+    return anotherVariable
+}
+
 
 /**
     Exercise 3:
     Make a function called ‘projectEulerProblem1’ that calculates the answer using
     mutable variables, a while loop, and returns the answer.
 */
-
+function projectEulerProblem1() {
+    //sum of all multiples of 3 or 5 below 1000
+    let max = 1000;
+    let sum = 0;
+    let val = 0;
+    while (val<max) {
+        if (val%3 === 0 || val%5 === 0) {
+            sum += val
+        }
+        val += 1
+    }
+    return sum
+}
 
 /**
     Exercise 4:
     Write a function called ‘alwaysTrue’ which always returns true, no matter what argument it is given.
 */
+function alwaysTrue(arg) {
+    return true
+}
+
 
 /**
     Write a function called imperativeSummer that takes two parameters: a function f, and a number n.  
     It should use an imperative loop to sum over the numbers from 1 up to (but not including) n,
     including the number x in the sum only if f(x) is true.
 */
+function imperativeSummer(f, n) {
+    let sum = 0;
+    for (let x = 1; x<n; x++) {
+        if (f(x)) {
+            sum += x
+        }
+    }
+    return sum
+}
 
 /**
     Write a function called sumTo that takes as parameter a number n and
     uses imperativeSummer and alwaysTrue to calculate the sum of all numbers
     from 1 up to (but not including) n.
 */
+function sumTo(n) {
+    return imperativeSummer(alwaysTrue, n)
+}
 
 /**
     Write a function called ‘isDivisibleByThreeOrFive’ which takes a number as parameter,
     tests if it is divisible by 3 or 5, returning true if it is.
 */
+function isDivisibleByThreeOrFive(n) {
+    return n%3===0 || n%5===0
+}
 
 /**
     Write a function called projectEulerProblem1UsingImperativeSummer 
     that uses your imperativeSummer and isDivisibleByThreeOrFive to
     again solve Project Euler Problem 1.  It should be one line of code!
 */
+function projectEulerProblem1UsingImperativeSummer() {
+    return imperativeSummer(isDivisibleByThreeOrFive, 1000)
+}
 
 /**
     Exercise 5:
@@ -78,10 +117,17 @@ browser window to run the test.
     for or any mutable variables (defined with let or var).
     Hint: use recursion!
 */
+function immutableSummer(f, n) {
+    return n-1 ? (f(n-1) ? n-1 : 0) + immutableSummer(f, n-1) : 0 //n-1 because [) indexing
+}
 
 /*
     Write a function called projectEulerProblem1UsingImmutableSummer 
     that uses your immutableSummer and isDivisibleByThreeOrFive to
     again solve Project Euler Problem 1.  It should be one line of code!
 */
-
+function projectEulerProblem1UsingImmutableSummer() {
+    return immutableSummer(isDivisibleByThreeOrFive, 1000)
+}
+console.log(projectEulerProblem1UsingImperativeSummer())
+console.log(projectEulerProblem1UsingImmutableSummer())
