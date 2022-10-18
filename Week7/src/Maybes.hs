f518cbd87ed5b60fb0f2aa71f9762c8f9c4c8639
Albert Chow <abhchow@gmail.com>
2022-10-18 13:59:41 +1100

    stuff

---
+++ Week7/src/Maybes.hs
@@ -0,0 +1,61 @@
+-- | Re-implementation of 'Maybe'-related functions.
+module Maybes where
+
+import Data.Maybe()
+
+-- $setup
+-- >>> ljust = [Just 1, Just 7, Just 3]
+-- >>> lnaught = [Just 3, Nothing, Just 8]
+
+-- | Simple boolean check for 'Maybe' values.
+--
+-- >>> isJust (Just 1)
+-- True
+--
+-- >>> isJust Nothing
+-- False
+isJust :: Maybe a -> Bool
+isJust (Just _) = True
+isJust Nothing = False
+
+
+-- | Inverse of 'isJust'.
+--
+-- >>> isNothing (Just 1)
+-- False
+--
+-- >>> isNothing Nothing
+-- True
+isNothing :: Maybe a -> Bool
+-- isNothing (Just _ ) = False
+-- isNothing Nothing = True
+isNothing a = not $ isJust a
+
+-- | Extract the value of a 'Just' but return a fallback in case of 'Nothing'.
+--
+-- >>> fromMaybe (Just 3) 7
+-- 3
+--
+-- >>> fromMaybe Nothing 7
+-- 7
+fromMaybe :: Maybe a -> a -> a
+fromMaybe (Just x) _ = x
+fromMaybe Nothing y = y
+
+-- | Gather 'Just' values in a list, filter the 'Nothing'.
+--
+-- >>> catMaybe ljust
+-- [1,7,3]
+--
+-- >>> catMaybe lnaught
+-- [3,8]
+
+catMaybe :: [Maybe a] -> [a]
+catMaybe [] = []
+catMaybe (x:xs) =
+    let rs = catMaybe xs in
+        case x of
+            Nothing -> rs
+            Just r -> r:rs
+
+-- catMaybe l = map (flip fromMaybe 1)  (filter isJust l)
